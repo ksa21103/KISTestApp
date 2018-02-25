d296912db277f429d225aed2cd5dedbf061dc3f7
Sergey Kopienko <a.kopienko@gmail.com>
2018-02-25 22:35:04 +0300

    Реализация класса Stopper

---
+++ KISTestApp/Stopper.cpp
@@ -0,0 +1,24 @@
+﻿#include "stdafx.h"
+#include "Stopper.h"
+
+Event Stopper::m_stopEvent(true, false);
+
+//----------------------------------------------------------------------------//
+void Stopper::SetStopState()
+{
+    m_stopEvent.SetEvent();
+}
+
+//----------------------------------------------------------------------------//
+bool Stopper::GetStopState() const
+{
+    return ::WaitForSingleObject(m_stopEvent.GetHandle(), 0) == WAIT_OBJECT_0;
+}
+
+//----------------------------------------------------------------------------//
+HANDLE Stopper::GetHandle() const
+{
+    return m_stopEvent.GetHandle();
+}
+
+//----------------------------------------------------------------------------//
---
+++ KISTestApp/Stopper.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "SynchPrimitives.h"
+
+////////////////////////////////////////////////////////////////////////////////
+// class Stopper - признак завершения работы
+class Stopper
+{
+public:
+
+    // Установить признак завершения работы
+    void    SetStopState();
+
+    // Получить признак завершения работы
+    bool    GetStopState() const;
+
+    // Получить описатель события, реализующего завершение работы
+    HANDLE  GetHandle() const;
+
+private:
+
+    static Event m_stopEvent;
+};
+
