0a7fe58bcb271b8535943ebd7702da74483d6e8b
Sergey Kopienko <a.kopienko@gmail.com>
2018-02-25 12:27:25 +0300

    Добавлен автогенерируемый идентификатор запроса

--- KISTestApp/Request.cpp
+++ KISTestApp/Request.cpp
@@ -1,9 +1,11 @@
 ﻿#include "stdafx.h"
 #include "Request.h"
 
+std::atomic<int> Request::s_nRequestGlobalIndex = { 0 };
 
 //----------------------------------------------------------------------------//
 Request::Request()
+    : m_nRequestIndex(s_nRequestGlobalIndex++)
 {
 }
 
