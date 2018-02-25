bebbee00c58f3ca4c97557510f5760339e0f6d35
Sergey Kopienko <a.kopienko@gmail.com>
2018-02-25 22:25:16 +0300

    Реализация очереди заданий

---
+++ KISTestApp/RequestsQueue.cpp
@@ -0,0 +1,106 @@
+﻿#include "stdafx.h"
+
+#include <array>
+
+#include "RequestsQueue.h"
+
+void DeleteRequest(Request* request);
+
+//----------------------------------------------------------------------------//
+RequestsQueue::RequestsQueue(size_t maxSize)
+    : m_maxSize(maxSize),
+      m_mutex_not_empty(true, false),
+      m_mutex_not_full(true, true)
+{
+}
+
+//----------------------------------------------------------------------------//
+RequestsQueue::~RequestsQueue()
+{
+    while (!m_queue.empty())
+    {
+        if (Request* request = m_queue.front())
+        {
+            DeleteRequest(request);
+        }
+        m_queue.pop();
+    }
+}
+
+//----------------------------------------------------------------------------//
+bool RequestsQueue::push(Stopper stopSignal, Request* request)
+{
+    const std::array<HANDLE, 2> handles{ stopSignal.GetHandle(), m_mutex_not_full.GetHandle() };
+
+    bool bProcessed(false);
+    while (!bProcessed)
+    {
+        switch (::WaitForMultipleObjects(handles.size(), handles.data(), FALSE, INFINITE))
+        {
+        case WAIT_OBJECT_0:
+            return false;
+
+        case WAIT_OBJECT_0 + 1:
+            {
+                CriticalSectionSentry csSentry(m_criticalSection);
+
+                if (m_maxSize == 0 || m_queue.size() < m_maxSize)
+                {
+                    m_queue.push(request);
+
+                    bProcessed = true;
+
+                    m_mutex_not_empty.SetEvent();
+
+                    if (m_maxSize > 0 && m_queue.size() >= m_maxSize)
+                    {
+                        m_mutex_not_full.ResetEvent();
+                    }
+                }
+            }
+            break;
+        }
+    }
+
+    return true;
+}
+
+//----------------------------------------------------------------------------//
+Request* RequestsQueue::pop(Stopper stopSignal)
+{
+    Request* result = nullptr;
+
+    const std::array<HANDLE, 2> handles{ stopSignal.GetHandle(), m_mutex_not_empty.GetHandle() };
+
+    while (result == nullptr)
+    {
+        switch (::WaitForMultipleObjects(handles.size(), handles.data(), FALSE, INFINITE))
+        {
+        case WAIT_OBJECT_0:
+            return nullptr;
+
+        case WAIT_OBJECT_0 + 1:
+            {
+                CriticalSectionSentry csSentry(m_criticalSection);
+
+                if (!m_queue.empty())
+                {
+                    result = m_queue.front();
+                    m_queue.pop();
+
+                    m_mutex_not_full.SetEvent();
+
+                    if (m_queue.empty())
+                    {
+                        m_mutex_not_empty.ResetEvent();
+                    }
+                }
+            }
+            break;
+        }
+    }
+
+    return result;
+}
+
+//----------------------------------------------------------------------------//
---
+++ KISTestApp/RequestsQueue.h
@@ -0,0 +1,66 @@
+﻿#pragma once
+
+#include <queue>
+#include <functional>
+
+#include "Request.h"
+#include "Stopper.h"
+
+////////////////////////////////////////////////////////////////////////////////
+// class RequestsQueue - очередь заданий
+class RequestsQueue
+{
+public:
+
+    // Конструктор
+    /*
+     * @param size_t maxSize - максимально допустимое количество элементов в очереди
+     *      Если 0 - длина очереди не ограниченна.
+     */
+    RequestsQueue(size_t maxSize);
+
+    // Положить задание в очередь
+    /*
+     * @param Stopper stopSignal - признак необходимости завершения работы
+     * @param Request* request - добавляемое в очередь задание
+     * @return bool - true, если задание было добавлено в очередь,
+     *      false - иначе (например в случае, если в процессе добавления задания
+     *          был установлен признак завершения работы приложения
+     * ВНИМАНИЕ: если очередь имеет ограниченную длину и заполнена, то
+     *           возврат из функции будет выполнен только в случае выполнения
+     *           добавления элемента в очередь или в случае установки признака
+     *           завершения работы приложения
+     */
+    bool        push(Stopper stopSignal, Request* request);
+
+    // Получить задание из очереди
+    /*
+     * @param Stopper stopSignal - признак необходимости завершения работы
+     * @return Request* - указатель на задание, извлеченное из очереди.
+     * ВНИМАНИЕ: возврат из функции будет выполнен только в случае получения задания
+     *           или в случае установки признака завершения работы приложения - в этом
+     *              случае будет возвращен нулевой указатель (nullptr)
+     */
+     Request*    pop (Stopper stopSignal);
+
+    // Обработка и удаление всех элементов очереди заданий
+    /**
+     * Внимание: функция не является потокобезопасной и работает без синхронизаций
+     *
+     * @param const std::function<void(Request*)>& fnc - функция для предварительной
+     *      обработки удаляемого задания
+     */
+    void        processAndEraseThreadUnsafe(const std::function<void(Request*)>& fnc);
+
+private:
+
+    const size_t    m_maxSize;
+
+    using RQueue = std::queue<Request*>;
+    RQueue          m_queue;
+
+    CriticalSection m_criticalSection;
+    Event           m_mutex_not_empty;
+    Event           m_mutex_not_full;
+};
+
