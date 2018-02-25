486200a3ff61b994c946f96679a4a3432ed06de5
Sergey Kopienko <a.kopienko@gmail.com>
2018-02-25 22:35:25 +0300

    Примитивы синхронизации

---
+++ KISTestApp/SynchPrimitives.cpp
@@ -0,0 +1,80 @@
+﻿#include "stdafx.h"
+
+#include "SynchPrimitives.h"
+
+//----------------------------------------------------------------------------//
+Mutex::Mutex()
+{
+    m_mutex = ::CreateMutex(nullptr, FALSE, nullptr);
+}
+
+//----------------------------------------------------------------------------//
+Mutex::~Mutex()
+{
+    ::DeleteObject(m_mutex);
+}
+
+//----------------------------------------------------------------------------//
+Mutex::operator HANDLE() const
+{
+    return m_mutex;
+}
+
+//----------------------------------------------------------------------------//
+CriticalSection::CriticalSection()
+{
+    ::InitializeCriticalSection(&m_cs);
+}
+
+//----------------------------------------------------------------------------//
+CriticalSection::~CriticalSection()
+{
+    ::DeleteCriticalSection(&m_cs);
+}
+
+//----------------------------------------------------------------------------//
+void CriticalSection::enter()
+{
+    ::EnterCriticalSection(&m_cs);
+}
+
+//----------------------------------------------------------------------------//
+void CriticalSection::leave()
+{
+    ::LeaveCriticalSection(&m_cs);
+}
+
+//----------------------------------------------------------------------------//
+Event::Event(bool bManualReset, bool bInitialState)
+{
+    m_hEvent = ::CreateEvent(nullptr,
+                             bManualReset  ? TRUE : FALSE,
+                             bInitialState ? TRUE : FALSE,
+                             nullptr);
+}
+
+//----------------------------------------------------------------------------//
+Event::~Event()
+{
+    ::DeleteObject(m_hEvent);
+}
+
+//----------------------------------------------------------------------------//
+void Event::SetEvent()
+{
+    ::SetEvent(m_hEvent);
+}
+
+//----------------------------------------------------------------------------//
+void Event::ResetEvent()
+{
+    ::ResetEvent(m_hEvent);
+}
+
+//----------------------------------------------------------------------------//
+HANDLE Event::GetHandle() const
+{
+    return m_hEvent;
+}
+
+//----------------------------------------------------------------------------//
---
+++ KISTestApp/SynchPrimitives.h
@@ -0,0 +1,89 @@
+﻿#pragma once
+
+#include <windows.h>
+
+////////////////////////////////////////////////////////////////////////////////
+// class Mutex - реализация мьютекса в парадигме RAII
+class Mutex
+{
+public:
+
+    Mutex();
+    ~Mutex();
+
+    operator HANDLE() const;
+
+private:
+
+    HANDLE m_mutex;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// class CriticalSection - реализация критической секции в парадигме RAII
+class CriticalSection
+{
+public:
+
+    CriticalSection();
+    ~CriticalSection();
+
+    void enter();
+    void leave();
+
+private:
+
+    CRITICAL_SECTION m_cs;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// class CriticalSectionSentry - вход в критическую секцию и выход из нее в парадигме RAII
+class CriticalSectionSentry
+{
+public:
+
+    CriticalSectionSentry(CriticalSection& cs)
+        : m_cs(cs)
+    {
+        m_cs.enter();
+    }
+
+    ~CriticalSectionSentry()
+    {
+        m_cs.leave();
+    }
+
+private:
+
+    CriticalSection & m_cs;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// class Event - реализация события в парадигме RAII
+class Event
+{
+public:
+
+    // Конструктор
+    /*
+     * @param bool bManualReset - признак автоматического сброса события
+     * @param bool bInitialState - начальное состояние события:
+     *      true - установлено, false - сброшено
+     */
+    Event(bool bManualReset, bool bInitialState);
+
+    // Деструктор
+    ~Event();
+
+    // Установить событие
+    void    SetEvent();
+
+    // Сбросить событие
+    void    ResetEvent();
+
+    // Получить описатель события
+    HANDLE  GetHandle() const;
+
+private:
+
+    HANDLE m_hEvent;
+};
