755dd0b0196788b7381844d2c684ee9fb2dcc045
Sergey Kopienko <a.kopienko@gmail.com>
2018-02-25 22:35:15 +0300

    Реализация класса Request

---
+++ KISTestApp/Request.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include <atomic>
+
+////////////////////////////////////////////////////////////////////////////////
+// class Request - задание для обработки
+class Request
+{
+public:
+
+	Request();
+	virtual ~Request();
+
+    // Получить идентификатор задания
+    const int ID() const
+        {   return m_nRequestIndex; }
+
+private:
+
+    // Идентификатор задания
+    const int               m_nRequestIndex;
+
+    // Счетчик заданий
+    static std::atomic<int> s_nRequestGlobalIndex;
+};
